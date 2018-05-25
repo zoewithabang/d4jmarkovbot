fbaf38c66da5a5c754b6e73b6f6dfcf713324153
zoewithabang <zoewithabang@gmail.com>
2018-05-25 22:34:11 +0100

    Removed old token parameter from run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 chmod +x target/d4jmarkovbot-1.0-SNAPSHOT-jar-with-dependencies.jar
-java -jar target/d4jmarkovbot-1.0-SNAPSHOT-jar-with-dependencies.jar $(cat .token)
+java -jar target/d4jmarkovbot-1.0-SNAPSHOT-jar-with-dependencies.jar
