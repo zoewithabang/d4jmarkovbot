1c146de8460221678ecda42cb1c11f55180319be
zoewithabang <zoewithabang@gmail.com>
2018-05-26 17:21:00 +0100

    Moved message validation to a separate method

--- src/main/java/com/github/zoewithabang/Command/GetAllMessagesFromUser.java
+++ src/main/java/com/github/zoewithabang/Command/GetAllMessagesFromUser.java
@@ -18,16 +18,32 @@ public class GetAllMessagesFromUser implements ICommand
     @Override
     public void execute(MessageReceivedEvent event, List<String> args)
     {
-        int argsSize = args.size();
-        
-        if(argsSize < 1)
+        if(!validateArgs(event, args))
         {
-            LOGGER.error("[MARKOVBOT] GetAllMessages expected 1 argument, found {}.", argsSize);
-            bot.sendMessage(event.getChannel(), "Error: Expected a single argument.");
             return;
         }
         
         LOGGER.debug("[MARKOVBOT] Executing GetAllMessagesFromUser for User '{}'", args.get(0));
-        bot.sendMessage(event.getChannel(), "This is where I would get messages!");
+        bot.sendMessage(event.getChannel(), "This is where I would get messages, given the arg " + args.get(0));
+        bot.sendMessage(event.getChannel(), "`This is where I would get messages, given the arg " + args.get(0) + "`");
+        
+        //check if arg is a user
+        //check if user has stored messages
+        //if yes, get latest message, then IChannel#getMessageHistoryFrom(LocalDateTime)
+        //if no, IChannel#getFullMessageHistory()
+    }
+    
+    private boolean validateArgs(MessageReceivedEvent event, List<String> args)
+    {
+        int argsSize = args.size();
+        
+        if(argsSize != 1)
+        {
+            LOGGER.warn("[MARKOVBOT] GetAllMessages expected 1 argument, found {}.", argsSize);
+            bot.sendMessage(event.getChannel(), "Error: Expected a single argument.");
+            return false;
+        }
+        
+        return true;
     }
 }
