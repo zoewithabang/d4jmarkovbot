3f25a39a72f5106cb21f47a0e8a5038ffeb43863
zoewithabang <zoewithabang@gmail.com>
2018-06-07 17:27:31 +0100

    few more old zerotube additions

---
+++ src/main/java/com/github/zoewithabang/command/GetZeroTube.java
@@ -0,0 +1,30 @@
+package com.github.zoewithabang.command;
+
+import com.github.zoewithabang.bot.IBot;
+import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+
+import java.util.List;
+import java.util.Properties;
+
+public class GetZeroTube implements ICommand
+{
+    public static final String command = "music";
+    private IBot bot;
+    private Properties botProperties;
+    private String url;
+    
+    public GetZeroTube(IBot bot, Properties botProperties)
+    {
+        this.bot = bot;
+        this.botProperties = botProperties;
+        url = botProperties.getProperty("cytubeurl");
+    }
+    
+    @Override
+    public void execute(MessageReceivedEvent event, List<String> args, boolean sendBotMessages)
+    {
+        //uD83C and uDFB5 make a musical note emoji
+        String message = "Tune in with me~ " + url + " \uD83C\uDFB5";
+        bot.sendMessage(event.getChannel(), message);
+    }
+}
