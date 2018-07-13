db214c54db79d96806e8ba0e2a54c0353d7de664
zoewithabang <zoewithabang@gmail.com>
2018-07-13 15:18:37 +0100

    Moved one existing and added new DiscordHelperTest methods.

--- src/test/groovy/com/github/zoewithabang/util/DiscordHelperTest.groovy
+++ src/test/groovy/com/github/zoewithabang/util/DiscordHelperTest.groovy
@@ -2,11 +2,46 @@ package com.github.zoewithabang.util
 
 import spock.lang.Specification
 import sx.blah.discord.handle.obj.IGuild
+import sx.blah.discord.handle.obj.IRole
 import sx.blah.discord.handle.obj.IUser
+
+import java.awt.Color
 
 class DiscordHelperTest extends Specification
 {
-    def "only args with valid user ids are added to user list"()
+    def "get a user from username markdown string"()
+    {
+        setup:
+        def guild = Mock(IGuild)
+        def user = Mock(IUser)
+
+        guild.getUsers() >> [user]
+        user.getStringID() >> "12345678901234567"
+
+        when:
+        def discordUser = DiscordHelper.getUserFromMarkdownId(guild, "<@12345678901234567>")
+
+        then:
+        discordUser.getStringID() == "12345678901234567"
+    }
+
+    def "get a user from nickname markdown string"()
+    {
+        setup:
+        def guild = Mock(IGuild)
+        def nicknamedUser = Mock(IUser)
+
+        guild.getUsers() >> [nicknamedUser]
+        nicknamedUser.getStringID() >> "123456789012345678"
+
+        when:
+        def discordUser = DiscordHelper.getUserFromMarkdownId(guild, "<@!123456789012345678>")
+
+        then:
+        discordUser.getStringID() == "123456789012345678"
+    }
+
+    def "only valid user ids are added to user list"()
     {
         setup:
         def guild = Mock(IGuild)
@@ -24,5 +59,43 @@ class DiscordHelperTest extends Specification
         userList.size() == 2
         userList.contains(realUser)
         userList.contains(realNicknamedUser)
+    }
+
+    def "get top role colour for user with one role"()
+    {
+        setup:
+        def user = Mock(IUser)
+        def guild = Mock(IGuild)
+        def role = Mock(IRole)
+
+        user.getRolesForGuild(guild) >> [role]
+        role.getColor() >> Color.MAGENTA
+
+        when:
+        def color = DiscordHelper.getColorOfTopRoleOfUser(user, guild)
+
+        then:
+        color == Color.MAGENTA
+    }
+
+    def "get top role colour for user with more than one role"()
+    {
+        setup:
+        def user = Mock(IUser)
+        def guild = Mock(IGuild)
+        def topRole = Mock(IRole)
+        def anotherRole = Mock(IRole)
+        def andAnotherRole = Mock(IRole)
+
+        user.getRolesForGuild(guild) >> [anotherRole, topRole, andAnotherRole]
+        topRole.getColor() >> Color.MAGENTA
+        anotherRole.getColor() >> Color.RED
+        andAnotherRole.getColor() >> Color.ORANGE
+
+        when:
+        def color = DiscordHelper.getColorOfTopRoleOfUser(user, guild)
+
+        then:
+        color == Color.MAGENTA
     }
 }
