3833ef4aa53ce80c6ee5dd5ee327c94a58afa1aa
zoewithabang <zoewithabang@gmail.com>
2018-07-22 17:38:06 +0100

    Updated SQL file for current build.

--- zerobot.sql
+++ zerobot.sql
@@ -1,8 +1,23 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` varchar(64) NOT NULL,
+  `tracked` tinyint(1) NOT NULL,
+  `permission_rank` int(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `aliases` (
   `alias` varchar(255) NOT NULL,
   `command` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`alias`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `commands`;
+CREATE TABLE `commands` (
+  `command` varchar(255) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `permission_rank` tinyint(3) unsigned NOT NULL,
+  PRIMARY KEY (`command`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `messages` (
@@ -19,10 +34,4 @@ CREATE TABLE IF NOT EXISTS `options` (
   `key` varchar(255) NOT NULL,
   `value` text NOT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` varchar(64) NOT NULL,
-  `tracked` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
