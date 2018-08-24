36b4a3a289f3b109765efff21cbe12952bae892d
zoewithabang <zoewithabang@gmail.com>
2018-08-24 17:30:41 +0100

    Version bump to 1.03

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 mvn clean install -q -Dmaven.test.skip=true
-chmod +x target/zerobot-1.02-jar-with-dependencies.jar
-java -jar target/zerobot-1.02-jar-with-dependencies.jar
+chmod +x target/zerobot-1.03-jar-with-dependencies.jar
+java -jar target/zerobot-1.03-jar-with-dependencies.jar
