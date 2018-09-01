8b73c92036e3b833768376bdc7a576a224672b7f
zoewithabang <zoewithabang@gmail.com>
2018-09-01 16:41:21 +0100

    Revert this for now.

--- src/test/groovy/com/github/zoewithabang/DatabaseSpecTrait.groovy
+++ src/test/groovy/com/github/zoewithabang/DatabaseSpecTrait.groovy
@@ -2,8 +2,9 @@ package com.github.zoewithabang
 
 import org.junit.Before
 
-trait DatabaseSpecTrait extends BotPropertiesTrait
+trait DatabaseSpecTrait
 {
+    Properties botProperties
     String dbUrl
     Properties dbProperties
     String dbDriver
@@ -11,6 +12,26 @@ trait DatabaseSpecTrait extends BotPropertiesTrait
     @Before
     def setupDatabaseSpec()
     {
+        botProperties = new Properties()
+        InputStream zeroBotPropertyStream = getClass().getClassLoader().getResourceAsStream("zerobot.properties")
+        if(zeroBotPropertyStream == null)
+        {
+            String[] keys = ["token", "prefix", "dbuser", "dbpassword", "dbaddress", "dbport", "dbdatabase"]
+            for(String key : keys)
+            {
+                String value = System.getProperty("zerobot" + key)
+                if(value == null)
+                {
+                    throw new NullPointerException("Null value for required key.")
+                }
+                botProperties.put(key, value)
+            }
+        }
+        else
+        {
+            botProperties.load(zeroBotPropertyStream)
+        }
+
         dbUrl = "jdbc:mysql://" + botProperties.getProperty("dbaddress") + ":" + botProperties.getProperty("dbport") + "/" + botProperties.getProperty("dbdatabase")
 
         dbProperties = new Properties()
